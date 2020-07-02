ac9044cbe653aee3bb311838893c59ec8048ee0d
Subhamp7 <Subham306952@gmail.com>
2020-07-02 20:21:31 +0530

    IMDB ratings using 2 URL

--- IMDB_rating/Scraping_IMDB_movies_Hindi.py
+++ IMDB_rating/Scraping_IMDB_movies_Hindi.py
@@ -12,23 +12,27 @@ try:
     
 except Exception as e:
     print("Importing libraries was unsuccessful",e)
-'''   
-https://www.imdb.com/search/title/?groups=top_1000&start=
-https://www.imdb.com/search/title/?groups=top_1000&start=51&ref_=adv_nxt
+''' 
+https://www.imdb.com/list/ls045285923/
+https://www.imdb.com/list/ls045285923/?sort=list_order,asc&st_dt=&mode=detail&page=2
 '''
+
 titles       = []
 years        = []
 imdb_ratings = []
 votes        = []
 count        = 0
 
-for index in range(1,1001,50):
+list_of_page=["https://www.imdb.com/list/ls045285923/",
+"https://www.imdb.com/list/ls045285923/?sort=list_order,asc&st_dt=&mode=detail&page=2"]
+
+for index in list_of_page:
     
-    URL="https://www.imdb.com/search/title/?groups=top_1000&start=" +str(index) + "&ref_=adv_nxt"
+    URL="https://www.imdb.com/list/ls045285923/"
     page=get(URL)
     print("Connected {} : {} ".format(count,page.status_code==200))
     soup=BeautifulSoup(page.text, 'html.parser')
-    movies=soup.find_all('div',class_="lister-item mode-advanced")
+    movies=soup.find_all('div',class_="lister-item mode-detail")
     count+=1
     for index in movies:
         title=index.h3.a.text
@@ -37,7 +41,7 @@ for index in range(1,1001,50):
         year=index.h3.find('span', class_="lister-item-year text-muted unbold").text
         years.append(year)
         
-        rating=index.strong.text
+        rating=index.find('span', class_="ipl-rating-star__rating").text
         imdb_ratings.append(rating)
         
         vote=index.find_all('span', attrs ={"name" : "nv"})
@@ -47,13 +51,11 @@ for index in range(1,1001,50):
 dict={"Titles" : titles, "Year" : years, "IMDB Ratings": imdb_ratings, "Votes": votes}
 
 final_dataset=pd.DataFrame(dict)
-print(final_dataset.dtypes)
 
-final_dataset["Year"]         =final_dataset["Year"].str.extract('(\d+)').astype(int)
-final_dataset["IMDB Ratings"] =final_dataset["IMDB Ratings"].astype(float)
-final_dataset['Votes']        =final_dataset['Votes'].str.replace(",","").astype(int)
-print(final_dataset.dtypes)
-    
-final_dataset.to_csv("IMDB Movie list.csv")
-    
+final_dataset["Year"]                   = final_dataset["Year"].str.extract('(\d+)').astype(int)
+final_dataset["IMDB Ratings"]           = final_dataset["IMDB Ratings"].astype(float)
+final_dataset["Votes"]                  = final_dataset["Votes"].str.replace(",","").astype(int)
+final_dataset["Percentage of Votes"]    = (final_dataset["Votes"]/final_dataset["Votes"].sum())*1000
 
+#to convert dataframe to csv
+#final_dataset.to_csv("IMDB_movies_list")
